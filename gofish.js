0d0f99a21072f4d7e6891ec36492477afe067dcc
Suren <surenenfiajyan@gmail.com>
2024-01-05 17:30:58 +0400

    Improvements

--- gofish.js
+++ gofish.js
@@ -308,10 +308,17 @@ export default class GoFish {
 	#generateOpponentGuess() {
 		const opponentCards = this.#getOpponentCards();
 		const guesses = Object.keys(opponentCards).filter(guess => opponentCards[guess]);
+		this.#shuffle(guesses);
 
 		if (this.#difficulty === 'hard') {
 			for (const guess of guesses) {
 				if (this.#minimumKnownYourCards[guess] === 3) {
+					return guess;
+				}
+			}
+
+			for (const guess of guesses) {
+				if (this.#minimumKnownYourCards[guess] === 2) {
 					return guess;
 				}
 			}
